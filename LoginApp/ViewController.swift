6ecda4574c6382b83bdd6f94ed6177e2cfb6b681
JijiMathew123 <jijijohnp@gmail.com>
2023-06-14 12:29:56 +0530

    UI design completed for Login Page

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var brownView: UIView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var topContainer: UIView!
+    @IBOutlet weak var innerTopContainer: UIView!
+    @IBOutlet weak var termsAndConditionsView: UIView!
+    
+    @IBOutlet weak var loginButtonView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello")
-        // Do any additional setup after loading the view.
     }
 
 
