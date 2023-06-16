237075b28d8f02b4acfeb2a4ee0d7dbec67328b8
JijiMathew123 <jijijohnp@gmail.com>
2023-06-16 19:25:10 +0530

    Added code for textfield setup

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var forgotLogin: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLoginButton()
     
     }
 func setupLoginButton()
@@ -29,6 +30,9 @@ func setupLoginButton()
         loginButton.layer.cornerRadius = 5.0
         // i am from Documents
         print("Hai")
+    }
+    func setupTextField(){
+        userName.text = "Jiji"
     }
 }
 
